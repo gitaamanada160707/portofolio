ac86ffaf22b448d3dd11572baf542afdc0e40dce
Nita Amanda <gitaamanada@gmail.com>
2024-10-10 10:13:40 +0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,81 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Nama saya Nita Amanda,saya seorang siswi SMK MEDIKA COM yang nantinya ingin bekerja di bidang Software Engineer atau Programmer.Sebagai seorang software engineer,tugas utama saya adalah mengembangkan perangkat lunak—baik itu aplikasi,sistem,maupun situs web—yang dapat digunakan untuk memecahkan berbagai masalah dan kebutuhan bisnis.
+Dalam pekerjaan saya,saya menggunakan berbagai bahasa pemrograman seperti Python,Java,JavaScript,atau C++ untuk menulis kode yang dapat menjalankan fungsionalitas tertentu.Selain itu, saya juga terlibat dalam proses desain,pengujian (testing),dan pemeliharaan perangkat lunak agar selalu berjalan dengan baik dan bebas dari bug.
+Untuk menjadi seorang software engineer yang efektif,saya terus belajar dan beradaptasi dengan teknologi baru,karena dunia pemrograman selalu berkembang dengan cepat.Profesi ini tidak hanya menuntut keahlian teknis,tetapi juga keterampilan komunikasi dan kolaborasi yang baik,karena kami sering bekerja dalam tim untuk mewujudkan proyek perangkat lunak.
+Melalui profesi ini,saya berkesempatan untuk menghadapi berbagai tantangan yang menarik,memecahkan masalah dengan kreativitas,dan tentunya memberikan dampak yang nyata bagi banyak orang,baik dalam bentuk aplikasi yang digunakan sehari-hari ataupun sistem operasi bisnis.`;
+
+export const ABOUT_TEXT = `selain semangat dari diri sendiri,saya sangat bangga dan berterima kasih atas dukungan luar biasa yang saya terima dari orang tua dalam perjalanan pendidikan saya di jurusan Rekayasa Perangkat Lunak (RPL).
+Orang tua saya selalu memberi semangat dan memotivasi untuk terus berprestasi,mereka selalu ada dan terus mengingatkan saya akan pentingnya pendidikan serta pengembangan diri. Dengan dukungan mereka,saya merasa lebih percaya diri untuk terus mengasah keterampilan dan mengejar karier di dunia teknologi,
+sekaligus menjaga prinsip etika kerja dan tanggung jawab yang mereka tanamkan.Saya sangat bangga memiliki orang tua yang selalu mendukung dan mendorong saya untuk meraih impian saya. Dukungan mereka adalah motivasi terbesar dalam perjalanan saya menuju sukses.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Staf IT Sistem Informasi Manajemen (SIM)",
+    company: "SMK Medikacom",
+    description: `Melakukan Pengolahan Data Internal Sekolah & Melakukan Troubleshooting dan Maintenance Sistem Aplikasi Sekolah`,
+    technologies: ["PHP", "Native", "HTML", "MySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Jl. Anatomi No.2 Komp. Dosen UNPAD, Cigadung, Cibeunying Kaler, Kota Bandung ",
+  phoneNo: "+62 812 9802 1258 ",
+  email: "rheyfauzan@gmail.com",
+};
